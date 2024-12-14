581eb4b049070de75c5f7e1fac04401bed9359c2
Aleksandr-1977 <vasilenkobox@gmail.com>
2024-12-14 18:10:43 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -5,3 +5,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector('button.change-color');
+const span = document.querySelector('span.color');
+button.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
